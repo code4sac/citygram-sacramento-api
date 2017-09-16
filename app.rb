3b66416c6f51a2989f026f7cc2ae6b274e69acfe
Gavin DeSchutter <gavin.deschutter@mac.com>
2017-09-16 12:38:01 -0700

    update endpoints for animal_care

--- app.rb
+++ app.rb
@@ -1,7 +1,14 @@
 require 'sinatra'
+require 'httparty'
+require 'json'
+require_relative './citygram/animal_care'
 
 set :bind, '0.0.0.0'
 
 get '/' do
-  "Hello World #{params[:name]}".strip
+  'Hello World!'
+end
+
+get '/animal_care' do
+  Citygram::AnimalCare.retrieve_records
 end
