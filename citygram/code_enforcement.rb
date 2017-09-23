7b367e43e1c6a0221c5cf6a8bc7731977cda779a
Enzo Arona <zzmail28@gmail.com>
2017-09-23 16:43:19 -0700

    Fixed indentations to match style guide

--- citygram/code_enforcement.rb
+++ citygram/code_enforcement.rb
@@ -4,11 +4,11 @@ module Citygram
     class CodeEnforcement
 
     ALLOWED_HIERARCHIES = [
-    'Code Enforcement : Environmental : Complaint',
-    'Code Enforcement : Environmental : Pest',
-    'Code Enforcement : Environmental : Stagnant Water',
-    'Code Enforcement : Vehicle On-Street : Potential   Hazard',
-    'Code Enforcement : Work Without Permit'
+      'Code Enforcement : Environmental : Complaint',
+      'Code Enforcement : Environmental : Pest',
+      'Code Enforcement : Environmental : Stagnant Water',
+      'Code Enforcement : Vehicle On-Street : Potential   Hazard',
+      'Code Enforcement : Work Without Permit'
     ].freeze
 
     def self.build_features(records)
@@ -34,13 +34,13 @@ module Citygram
     end
 
     def self.retrieve_records(offset = 0)
-        url = Citygram::Url.build('Code Enforcement', 0)
+      url = Citygram::Url.build('Code Enforcement', 0)
       response = JSON.parse(HTTParty.get(url).body)
       records = response['features']
 
       features = build_features(records)
 
-        next_page_url =  Citygram::Url.build('Code Enforcement', offset + 1000) if response['exceededTransferLimit']
+      next_page_url =  Citygram::Url.build('Code Enforcement', offset + 1000) if response['exceededTransferLimit']
       [{'features' => features.compact }.to_json, next_page_url]
     end
   end
