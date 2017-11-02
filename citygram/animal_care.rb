cc19bf3903d22d952a18e3ea337559be7217af33
Gavin DeSchutter <gavin.deschutter@mac.com>
2017-11-01 20:32:51 -0700

    limit mapping to one week ago

--- citygram/animal_care.rb
+++ citygram/animal_care.rb
@@ -3,10 +3,15 @@ require_relative './url'
 module Citygram
   class AnimalCare
     def self.build_features(records)
+      one_week_ago = (Time.now.to_i - (7*24*60*60)) * 1000
+
       records.map do |record|
         lat = record['attributes']['Latitude'].to_f
         lng = record['attributes']['Longitude'].to_f
+        created_at = record['attributes']['DateCreated'].to_i
+
         next if lat == 0.0 || lng == 0.0
+        next if created_at < one_week_ago
 
         title = record['attributes']['CategoryHierarchy']
         {
@@ -29,6 +34,7 @@ module Citygram
       features = build_features(records)
 
       next_page_url =  Citygram::Url.build('Animal care', offset + 1000) if response['exceededTransferLimit']
+
       [{'features' => features.compact }.to_json, next_page_url]
     end
   end
