2f4abc041a273fd5c4a6b4d86f67e6498cde2d25
Gavin DeSchutter <gavin.deschutter@mac.com>
2017-11-12 11:04:51 -0800

    sewer endpoint, constantize three days ago var

--- citygram/animal_care.rb
+++ citygram/animal_care.rb
@@ -1,37 +1,14 @@
 require_relative './url'
+require_relative './feature'
 
 module Citygram
   class AnimalCare
-    def self.build_features(records)
-      one_week_ago = (Time.now.to_i - (7*24*60*60)) * 1000
-
-      records.map do |record|
-        lat = record['attributes']['Latitude'].to_f
-        lng = record['attributes']['Longitude'].to_f
-        created_at = record['attributes']['DateCreated'].to_i
-
-        next if lat == 0.0 || lng == 0.0
-        next if created_at < one_week_ago
-
-        title = record['attributes']['CategoryHierarchy']
-        {
-          'id'          => record['attributes']['GlobalID'],
-          'type'        => 'Feature',
-          'properties'  => record.merge('title' => title),
-          'geometry'    => {
-            'type'        => 'Point',
-            'coordinates' =>  [lng, lat]
-          }
-        }
-      end
-    end
-
     def self.retrieve_records(offset = 0)
       url = Citygram::Url.build('Animal care', offset)
       response = JSON.parse(HTTParty.get(url).body)
       records = response['features']
 
-      features = build_features(records)
+      features = Citygram::Feature.build(records)
 
       next_page_url =  Citygram::Url.build('Animal care', offset + 1000) if response['exceededTransferLimit']
 
--- citygram/urban_forestry.rb
+++ citygram/urban_forestry.rb
@@ -6,35 +6,13 @@ module Citygram
 	ALLOWED_HIERARCHIES = [
   	'Urban Forestry : Inspection'
   ].freeze
-      
-	def self.build_features(records)
-		records.map do |record|
-      lat = record['attributes']['Latitude'].to_f
-      lng = record['attributes']['Longitude'].to_f
-			next if lat == 0.0 || lng == 0.0
-
-      category_hierarchy = record['attributes']['CategoryHierarchy']
-      next unless ALLOWED_HIERARCHIES.include?(category_hierarchy)
-
-      title = record['attributes']['CategoryHierarchy']
-      {
-        'id'          => record['attributes']['GlobalID'],
-        'type'        => 'Feature',
-        'properties'  => record.merge('title' => title),
-        'geometry'    => {
-        	'type'        => 'Point',
-        	'coordinates' =>  [lng, lat]
-          }
-        }
-      end
-    end
 
     def self.retrieve_records(offset = 0)
       url = Citygram::Url.build('Urban Forestry', 0)
       response = JSON.parse(HTTParty.get(url).body)
       records = response['features']
 
-      features = build_features(records)
+      features = Citygram::Feature.build(records, ALLOWED_HIERARCHIES)
 
       next_page_url =  Citygram::Url.build('Urban Forestry', offset + 1000) if response['exceededTransferLimit']
       [{'features' => features.compact }.to_json, next_page_url]
